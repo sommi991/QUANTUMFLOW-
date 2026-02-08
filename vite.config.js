976b3d3a6d79820a5074ca3c9977c48ab878e05f
sommi991 <odumegwusomto9@gmail.com>
2026-02-08 13:29:47 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',
-  server: {
-    port: 3000,
-    open: true
+  base: './',  // Use relative paths
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    }
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
   }
 })
